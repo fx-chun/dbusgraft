1a9169c06167502bdab441dac51be966bec71d49
Andrew Baxter <>
2025-04-17 13:07:39 +0900

    Update build to handle git dep

--- source/package.nix
+++ source/package.nix
@@ -1,15 +1,15 @@
-{ pkgs }:
+{ lib, pkgs }:
 pkgs.callPackage (
-  { }:
-  let
-    naersk =
-      pkgs.callPackage
-        (fetchTarball "https://github.com/nix-community/naersk/archive/378614f37a6bee5a3f2ef4f825a73d948d3ae921.zip")
-        { };
-  in
-  naersk.buildPackage {
-    pname = "dbusgraft";
-    root = ./.;
+  { rustPlatform }:
+  rustPlatform.buildRustPackage {
+    name = "dbusgraft";
+    src = ./.;
+    cargoLock = {
+      lockFile = ./Cargo.lock;
+      outputHashes = {
+        "zbus-5.5.0" = "sha256-4iMmDzygZvSKxps1/8RNJRfWQktskVw4FYEPAt7D5Y8=";
+      };
+    };
     nativeBuildInputs = [
       # pkgs.pkg-config
       # pkgs.llvmPackages.libclang
